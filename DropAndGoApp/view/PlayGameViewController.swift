eeb848a0d8715d25039612eaf300a55884cafbeb
Dantoft Alex <adantoft@gmail.com>
2016-03-12 22:17:36 -0600

    updated display board to be for loop

--- DropAndGoApp/view/PlayGameViewController.swift
+++ DropAndGoApp/view/PlayGameViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PlayGameViewController: UIViewController {
-
+    
     @IBOutlet weak var player1Display: UILabel!
     
     @IBOutlet weak var player2Display: UILabel!
@@ -24,22 +24,28 @@ class PlayGameViewController: UIViewController {
     var array = Array(count: 9, repeatedValue: Array(count: 9, repeatedValue: 0))
     
     
-
+    
     @IBAction func switchToMainMenu(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-
     
     
-   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         drawBoard()
+        
+        //        for n in objects {
+        //            n.backgroundColor = UIColor.whiteColor()
+        //        }
 
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -48,7 +54,15 @@ class PlayGameViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         player1Display.text = player1 + ": 0"
         player2Display.text = player2 + ": 0"
-       
+        
+        for var i = 0; i < 81; i++ {
+            if i % 2 == 0 {
+                objects[i].backgroundColor = UIColor.redColor()
+                objects[i].text = "*"
+            }
+            
+        }
+        
     }
     
     
@@ -60,9 +74,9 @@ class PlayGameViewController: UIViewController {
             /*
             target.message = "From YellowViewController"
             if let text = textField.text {
-                target.message += "\nMessage: \(text)"
+            target.message += "\nMessage: \(text)"
             }
-*/
+            */
         }
     }
     
@@ -81,8 +95,9 @@ class PlayGameViewController: UIViewController {
         var row = 1
         var col = 1
         
-        while(col-1 < numCols) {
-            repeat {
+        
+        for var i = 0; i < 9; i++ {//vertical score evaluation
+            for var j = 0; j < 9; j++ {
                 let gameBoard = UILabel()
                 gameBoard.backgroundColor = UIColor.blueColor()
                 gameBoard.frame = CGRect(x: xOffSet, y: yOffSet, width: 24, height: 24)
@@ -103,35 +118,64 @@ class PlayGameViewController: UIViewController {
                     gameBoard.text = "O"
                 }
                 view.addSubview(gameBoard)
-                //objects.append(gameBoard)
-                xOffSet += 26
-                row++
-            } while row-1 < numRows
-            yOffSet += 26
-            xOffSet = 26
-            row = 1
-            col++
+                objects.append(gameBoard)
+                yOffSet += 26
+            }
+            yOffSet = 96
+            xOffSet += 26
         }
+        
+        //        while(col-1 < numCols) {
+        //            repeat {
+        //                let gameBoard = UILabel()
+        //                gameBoard.backgroundColor = UIColor.blueColor()
+        //                gameBoard.frame = CGRect(x: xOffSet, y: yOffSet, width: 24, height: 24)
+        //                //var i = board!.board[row][col]
+        //                var i = array[row - 1][col - 1]
+        //                if i == 0 {
+        //                    print("print = 0")
+        //                } else if (i == 1){
+        //                    gameBoard.textColor = UIColor.whiteColor()
+        //                    gameBoard.textAlignment = .Center
+        //                    gameBoard.font = UIFont.systemFontOfSize(20)
+        //                    gameBoard.text = "X"
+        //                    print("print = 1")
+        //                } else if (i == 2){
+        //                    gameBoard.textColor = UIColor.whiteColor()
+        //                    gameBoard.textAlignment = .Center
+        //                    gameBoard.font = UIFont.systemFontOfSize(20)
+        //                    gameBoard.text = "O"
+        //                }
+        //                view.addSubview(gameBoard)
+        //                objects.append(gameBoard)
+        //                xOffSet += 26
+        //                row++
+        //            } while row-1 < numRows
+        //            yOffSet += 26
+        //            xOffSet = 26
+        //            row = 1
+        //            col++
+        //        }
         
     }
     
-
     
- 
     
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
